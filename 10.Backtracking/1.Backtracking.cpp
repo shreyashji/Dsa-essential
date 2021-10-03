55bc152ae78466aba1a1bb45dc3eecc9a6762120
Shreyash Choudhary <shreyashchoudhary7789@gmail.com>
2021-10-03 21:32:10 +0530

    finding subset cpp program

--- 10.Backtracking/1.Backtracking.cpp
+++ 10.Backtracking/1.Backtracking.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
+#include<vector>
 using namespace std;
 //algorithimic technique
 //recursivelty tring to build solution
 //searching the entire search space to solve a computation problem
 //problem types :-
 //decision problem,optimization problem, enumeration probblem
-void printarray(int *arr,int n){
+void printarray(vector<int> arr,int n){
 		for(int i=0; i<n; i++){
 			cout<<arr[i]<<" ";
 		}
 		cout<<endl;
 }
-void fillarray(int *arr,int i ,int n,int val){
+//void fillarray(int *arr,int i ,int n,int val)
+//vector is pass by 
+void fillarray(vector<int> & arr,int i ,int n,int val){
 	//base case
 	if(i==n){
 		//print our array
@@ -28,7 +31,7 @@ void fillarray(int *arr,int i ,int n,int val){
 
 int main() {
 	// backtracking on array code
-	int arr[100]= {0};
+	vector<int> arr(100,0);
 	int n;
 	cin>>n;
 	fillarray(arr,0,n,1);
